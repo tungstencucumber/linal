afee07e9c1352a7db79ff14716fbb1f5590199ad
tungstencucumber <44pyriaiev44@gmail.com>
2020-03-25 20:11:17 +0300

    Done building with CMake.

---
+++ build/CMakeFiles/linal.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/node/workspace/linal/Main.cpp" "/home/node/workspace/linal/build/CMakeFiles/linal.dir/Main.o"
+  "/home/node/workspace/linal/Matrix3D.cpp" "/home/node/workspace/linal/build/CMakeFiles/linal.dir/Matrix3D.o"
+  "/home/node/workspace/linal/Vector3D.cpp" "/home/node/workspace/linal/build/CMakeFiles/linal.dir/Vector3D.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/linal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linal.dir/Main.o"
+  "CMakeFiles/linal.dir/Matrix3D.o"
+  "CMakeFiles/linal.dir/Vector3D.o"
+  "linal.pdb"
+  "linal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
